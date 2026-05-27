90030090013596e3b7b6110011e3a87042fa1e9b
Sharan <sharan.b3333@gmail.com>
2026-05-27 10:21:57 +0530

    Added protected routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
-  return <Dashboard />;
+
+  const token = localStorage.getItem("token");
+
+  return (
+    <>
+      {token ? <Dashboard /> : <Login />}
+    </>
+  );
 }
 
 export default App;
