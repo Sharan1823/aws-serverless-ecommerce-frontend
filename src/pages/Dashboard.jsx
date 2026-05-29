31d3be437ed5689869701bc344efe4765f3837eb
Sharan <sharan.b3333@gmail.com>
2026-05-29 18:52:35 +0530

    added color

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -210,7 +210,7 @@ function Dashboard() {
           Create Order
         </h2>
 
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
 
           <input
             type="text"
